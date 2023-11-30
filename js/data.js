8d74474c7d6df9d82734793ac2bd41d9b8648aef
David <okekedavid1333@gmail.com>
2023-11-30 14:24:37 +0100

    all angels bug fix2

---
+++ js/data.js
@@ -0,0 +1,186 @@
+
+let cartItems = [
+    {
+      productId: Math.random(),
+      productImage:
+        "https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YjAwMjVmYjUt/YjAwMjVmYjUt-YmMzZGJjMzYt-w379._SY304_CB410321238_.jpg",
+      productName: "Alexa Speaker",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://image.api.playstation.com/pr/bam-art/148/722/651dbb59-7d61-448c-ac74-512fb8d3d080.jpg?w=620&thumb=false",
+      productName: "HP 15",
+      productPrice: 9000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://m.media-amazon.com/images/I/71SrEBVLj5L._AC_UL480_FMwebp_QL65_.jpg",
+      productName: "Dual Shock 3",
+      productPrice: 2500,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://m.media-amazon.com/images/I/511Nqd8mKcL._AC_UL480_FMwebp_QL65_.jpg",
+      productName: "Egronomic Back Rest",
+      productPrice: 3000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP EliteBook",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/72/7790731/1.jpg?1476",
+      productName: "Shayo",
+      productPrice: 9000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://m.media-amazon.com/images/I/81NhEuQzTyL._SL1500_.jpg",
+      productName: "Sonic Hedgehog",
+      productPrice: 2500,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://m.media-amazon.com/images/I/61nbxA368iL._AC_UY327_FMwebp_QL65_.jpg",
+      productName: "NFL Madden",
+      productPrice: 3000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://m.media-amazon.com/images/I/71Wltf78aJL._AC_UY327_FMwebp_QL65_.jpg",
+      productName: "FC 24",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/72/7790731/1.jpg?1476",
+      productName: "HP 15",
+      productPrice: 9000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP Pavilion",
+      productPrice: 2500,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP Spectre",
+      productPrice: 3000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP EliteBook",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/72/7790731/1.jpg?1476",
+      productName: "HP 15",
+      productPrice: 9000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP Spectre",
+      productPrice: 3000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP EliteBook",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+  
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP EliteBook",
+      productPrice: 5000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/72/7790731/1.jpg?1476",
+      productName: "HP 15",
+      productPrice: 9000,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP Pavilion",
+      productPrice: 2500,
+      productQty: 1,
+      like: "no",
+    },
+    {
+      productId: Math.random(),
+      productImage:
+        "https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/86/370383/1.jpg?1055",
+      productName: "HP Spectre",
+      productPrice: 3000,
+      productQty: 1,
+      like: "no",
+    },
+  ];
+
+  console.log(cartItems[1].productName)
